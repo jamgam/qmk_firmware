960b485f153b79cf204c04d5043050ea0b9f4b57
Bryan Liang <bryanliang1995@gmail.com>
2024-09-23 21:41:19 -0700

    Change var names to be shorter

--- keyboards/handwired/jamgam/4x6_3/keymaps/jamgam/keymap.c
+++ keyboards/handwired/jamgam/4x6_3/keymaps/jamgam/keymap.c
@@ -1,13 +1,14 @@
 // Copyright 2023 Adam Tombleson (@rekarnar)
 // SPDX-License-Identifier: GPL-2.0-or-later
 #include ".build/obj_handwired_jamgam_4x6_3_jamgam/src/default_keyboard.h"
+#include "keycodes.h"
 #include QMK_KEYBOARD_H
 
-enum custom_keycodes {  TO_WIN = SAFE_RANGE, TO_MAC,PREV_WORD, NEXT_WORD, DELETE_WORD, SELECT_LINE, MAIN_MODIFIER };
+enum custom_keycodes {  TO_WIN = SAFE_RANGE, TO_MAC,PREV_WORD, NEXT_WORD, DELETE_WORD, SELECT_LINE, MOD };
 
 enum layer_names {
     _BASE,
-    _SYMBOL,
+    _SYM,
     _MISC,
 };
 
@@ -16,23 +17,23 @@ enum combos {
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-//    ┌────────────────┬───┬──────┬──────┬──────┬───────────────┐                         ┌───────────────────┬─────┬──────┬────┬───┬──────┐
-//    │ LT(_MISC, tab) │ q │  w   │  f   │  p   │       b       │                         │         j         │  l  │  u   │ y  │ ; │  \   │
-//    ├────────────────┼───┼──────┼──────┼──────┼───────────────┤                         ├───────────────────┼─────┼──────┼────┼───┼──────┤
-//    │ LT(_MISC, esc) │ a │  r   │  s   │  t   │       g       │                         │         m         │  n  │  e   │ i  │ o │  '   │
-//    ├────────────────┼───┼──────┼──────┼──────┼───────────────┤                         ├───────────────────┼─────┼──────┼────┼───┼──────┤
-//    │      lctl      │ z │  x   │  c   │  d   │       v       │                         │         k         │  h  │  ,   │ .  │ / │ lgui │
-//    └────────────────┴───┼──────┼──────┼──────┴───────────────┘                         └───────────────────┴─────┼──────┼────┼───┴──────┘
-//                         │ left │ rght │                                                                          │ down │ up │
-//                         └──────┴──────┼──────┬───────────────┬───────────────┐   ┌─────┬───────────────────┬─────┼──────┴────┘
-//                                       │ lalt │ OSM(MOD_LSFT) │ MAIN_MODIFIER │   │ ent │ LT(_SYMBOL, bspc) │ spc │
-//                                       └──────┴───────────────┴───────────────┘   └─────┴───────────────────┴─────┘
+//    ┌────────────────┬───┬──────┬──────┬──────┬───────────────┐               ┌────────────────┬─────┬──────┬────┬───┬──────┐
+//    │ LT(_SYM, tab)  │ q │  w   │  f   │  p   │       b       │               │       j        │  l  │  u   │ y  │ ; │  \   │
+//    ├────────────────┼───┼──────┼──────┼──────┼───────────────┤               ├────────────────┼─────┼──────┼────┼───┼──────┤
+//    │ LT(_MISC, esc) │ a │  r   │  s   │  t   │       g       │               │       m        │  n  │  e   │ i  │ o │  '   │
+//    ├────────────────┼───┼──────┼──────┼──────┼───────────────┤               ├────────────────┼─────┼──────┼────┼───┼──────┤
+//    │      rctl      │ z │  x   │  c   │  d   │       v       │               │       k        │  h  │  ,   │ .  │ / │ lgui │
+//    └────────────────┴───┼──────┼──────┼──────┴───────────────┘               └────────────────┴─────┼──────┼────┼───┴──────┘
+//                         │ left │ rght │                                                             │ down │ up │
+//                         └──────┴──────┼──────┬───────────────┬─────┐   ┌─────┬────────────────┬─────┼──────┴────┘
+//                                       │ lalt │ OSM(MOD_LSFT) │ MOD │   │ ent │ LT(_SYM, bspc) │ spc │
+//                                       └──────┴───────────────┴─────┘   └─────┴────────────────┴─────┘
 [_BASE] = LAYOUT(
-  LT(_MISC, KC_TAB) , KC_Q , KC_W    , KC_F     , KC_P    , KC_B          ,                                KC_J                 , KC_L   , KC_U    , KC_Y   , KC_SCLN , KC_BSLS,
-  LT(_MISC, KC_ESC) , KC_A , KC_R    , KC_S     , KC_T    , KC_G          ,                                KC_M                 , KC_N   , KC_E    , KC_I   , KC_O    , KC_QUOT,
-  KC_LCTL           , KC_Z , KC_X    , KC_C     , KC_D    , KC_V          ,                                KC_K                 , KC_H   , KC_COMM , KC_DOT , KC_SLSH , KC_LGUI,
-                             KC_LEFT , KC_RIGHT ,                                                                                          KC_DOWN , KC_UP                     ,
-                                                  KC_LALT , OSM(MOD_LSFT) , MAIN_MODIFIER ,     KC_ENTER , LT(_SYMBOL, KC_BSPC) , KC_SPC
+  LT(_SYM, KC_TAB)  , KC_Q , KC_W    , KC_F     , KC_P    , KC_B          ,                      KC_J              , KC_L   , KC_U    , KC_Y   , KC_SCLN , KC_BSLS,
+  LT(_MISC, KC_ESC) , KC_A , KC_R    , KC_S     , KC_T    , KC_G          ,                      KC_M              , KC_N   , KC_E    , KC_I   , KC_O    , KC_QUOT,
+  KC_RCTL           , KC_Z , KC_X    , KC_C     , KC_D    , KC_V          ,                      KC_K              , KC_H   , KC_COMM , KC_DOT , KC_SLSH , KC_LGUI,
+                             KC_LEFT , KC_RIGHT ,                                                                             KC_DOWN , KC_UP                     ,
+                                                  KC_LALT , OSM(MOD_LSFT) , MOD ,     KC_ENTER , LT(_SYM, KC_BSPC) , KC_SPC
 ),
 
 //    ┌─────────┬───┬───────────┬───────────┬─────┬─────┐               ┌──────┬─────┬──────┬────┬───┬─────────┐
@@ -46,7 +47,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 //                  └───────────┴───────────┼─────┬─────┬─────┐   ┌─────┬──────┬─────┼──────┴────┘
 //                                          │     │     │     │   │     │      │     │
 //                                          └─────┴─────┴─────┘   └─────┴──────┴─────┘
-[_SYMBOL] = LAYOUT(
+[_SYM] = LAYOUT(
   KC_VOLU      , KC_LCBR , KC_LPRN   , KC_RPRN   , KC_RCBR , KC_INS  ,                         KC_PGUP , KC_TILDE , KC_UNDS , KC_GRV  , KC_COLN , KC_MUTE,
   KC_VOLD      , KC_EXLM , KC_AT     , KC_HASH   , KC_DLR  , KC_PERC ,                         KC_CIRC , KC_AMPR  , KC_ASTR , KC_EQL  , KC_MINS , KC_DQUO,
   LGUI(KC_GRV) , KC_LT   , KC_LBRC   , KC_RBRC   , KC_GT   , KC_DEL  ,                         KC_PGDN , KC_BSLS  , KC_PIPE , KC_PLUS , KC_QUES , QK_BOOT,
@@ -54,23 +55,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                    KC_TRNS , KC_TRNS , KC_TRNS ,     KC_TRNS , KC_TRNS , KC_TRNS
 ),
 
-//    ┌─────┬─────┬────────┬────────┬─────┬─────────┐               ┌─────┬─────┬───┬───┬─────┬─────┐
-//    │     │ f12 │   f7   │   f8   │ f9  │ EE_CLR  │               │     │  7  │ 8 │ 9 │  *  │  /  │
-//    ├─────┼─────┼────────┼────────┼─────┼─────────┤               ├─────┼─────┼───┼───┼─────┼─────┤
-//    │     │ f11 │   f4   │   f5   │ f6  │         │               │     │  4  │ 5 │ 6 │     │  -  │
-//    ├─────┼─────┼────────┼────────┼─────┼─────────┤               ├─────┼─────┼───┼───┼─────┼─────┤
-//    │     │ f10 │   f1   │   f2   │ f3  │ QK_BOOT │               │  0  │  1  │ 2 │ 3 │  +  │     │
-//    └─────┴─────┼────────┼────────┼─────┴─────────┘               └─────┴─────┼───┼───┼─────┴─────┘
-//                │ TO_WIN │ TO_MAC │                                           │ 0 │ . │
-//                └────────┴────────┼─────┬─────────┬─────┐   ┌─────┬─────┬─────┼───┴───┘
-//                                  │     │         │     │   │     │     │     │
-//                                  └─────┴─────────┴─────┘   └─────┴─────┴─────┘
+//    ┌─────┬─────┬─────┬─────┬─────┬─────────┐               ┌─────┬─────┬───┬───┬─────┬─────┐
+//    │     │ f12 │ f7  │ f8  │ f9  │ EE_CLR  │               │     │  7  │ 8 │ 9 │  *  │  /  │
+//    ├─────┼─────┼─────┼─────┼─────┼─────────┤               ├─────┼─────┼───┼───┼─────┼─────┤
+//    │     │ f11 │ f4  │ f5  │ f6  │         │               │     │  4  │ 5 │ 6 │     │  -  │
+//    ├─────┼─────┼─────┼─────┼─────┼─────────┤               ├─────┼─────┼───┼───┼─────┼─────┤
+//    │     │ f10 │ f1  │ f2  │ f3  │ QK_BOOT │               │  0  │  1  │ 2 │ 3 │  +  │     │
+//    └─────┴─────┼─────┼─────┼─────┴─────────┘               └─────┴─────┼───┼───┼─────┴─────┘
+//                │     │     │                                           │ 0 │ . │
+//                └─────┴─────┼─────┬─────────┬─────┐   ┌─────┬─────┬─────┼───┴───┘
+//                            │     │         │     │   │     │     │     │
+//                            └─────┴─────────┴─────┘   └─────┴─────┴─────┘
 [_MISC] = LAYOUT(
-  KC_TRNS , KC_F12 , KC_F7  , KC_F8  , KC_F9   , EE_CLR  ,                         KC_TRNS , KC_7    , KC_8 , KC_9   , KC_ASTR , KC_SLASH,
-  KC_TRNS , KC_F11 , KC_F4  , KC_F5  , KC_F6   , KC_TRNS ,                         KC_TRNS , KC_4    , KC_5 , KC_6   , KC_TRNS , KC_MINS ,
-  KC_TRNS , KC_F10 , KC_F1  , KC_F2  , KC_F3   , QK_BOOT ,                         KC_0    , KC_1    , KC_2 , KC_3   , KC_PLUS , KC_TRNS ,
-                     TO_WIN , TO_MAC ,                                                                 KC_0 , KC_DOT                     ,
-                                       KC_TRNS , KC_TRNS , KC_TRNS ,     KC_TRNS , KC_TRNS , KC_TRNS
+  KC_TRNS , KC_F12 , KC_F7   , KC_F8   , KC_F9   , EE_CLR  ,                         KC_TRNS , KC_7    , KC_8 , KC_9   , KC_ASTR , KC_SLASH,
+  KC_TRNS , KC_F11 , KC_F4   , KC_F5   , KC_F6   , KC_TRNS ,                         KC_TRNS , KC_4    , KC_5 , KC_6   , KC_TRNS , KC_MINS ,
+  KC_TRNS , KC_F10 , KC_F1   , KC_F2   , KC_F3   , QK_BOOT ,                         KC_0    , KC_1    , KC_2 , KC_3   , KC_PLUS , KC_TRNS ,
+                     KC_TRNS , KC_TRNS ,                                                                 KC_0 , KC_DOT                     ,
+                                         KC_TRNS , KC_TRNS , KC_TRNS ,     KC_TRNS , KC_TRNS , KC_TRNS
 )
 };
 
@@ -117,7 +118,7 @@ void main_modifier(bool pressed) {
     }
 }
 
-const uint16_t PROGMEM delete_word_combo[] = {LT(_SYMBOL, KC_BSPC), KC_SPC, COMBO_END};
+const uint16_t PROGMEM delete_word_combo[] = {LT(_SYM, KC_BSPC), KC_SPC, COMBO_END};
 combo_t key_combos[] = {
     [DELETE_WORD_COMBO] = COMBO(delete_word_combo, KC_NO),
 };
@@ -142,7 +143,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         case SELECT_LINE:
             select_line(pressed);
             return false;
-        case MAIN_MODIFIER:
+        case MOD:
             main_modifier(pressed);
             return true;
         default:
