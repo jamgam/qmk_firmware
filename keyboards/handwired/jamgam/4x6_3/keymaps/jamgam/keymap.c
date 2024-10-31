fcdaaf190a62b191959f90dbf878f549cffc0c81
Bryan Liang <bryanliang1995@gmail.com>
2024-10-31 08:01:13 -0700

    Add 3x7 keymap

--- keyboards/handwired/jamgam/4x6_3/keymaps/jamgam/keymap.c
+++ keyboards/handwired/jamgam/4x6_3/keymaps/jamgam/keymap.c
@@ -1,5 +1,6 @@
 // Copyright 2023 Adam Tombleson (@rekarnar)
 // SPDX-License-Identifier: GPL-2.0-or-later
+#include "keycodes.h"
 #include QMK_KEYBOARD_H
 
 enum custom_keycodes {  TO_WIN = SAFE_RANGE, TO_MAC,PREV_WORD, NEXT_WORD, DELETE_WORD, SELECT_LINE, MOD };
@@ -61,7 +62,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 //    ├─────┼─────┼─────┼─────┼─────┼─────────┤   ├─────┼─────┼─────┼───┼───┼─────┤
 //    │     │ f11 │ f4  │ f5  │ f6  │         │   │     │  4  │  5  │ 6 │ - │     │
 //    ├─────┼─────┼─────┼─────┼─────┼─────────┤   ├─────┼─────┼─────┼───┼───┼─────┤
-//    │     │ f10 │ f1  │ f2  │ f3  │ QK_BOOT │   │  0  │  1  │  2  │ 3 │ + │     │
+//    │     │ f10 │ f1  │ f2  │ f3  │ QK_BOOT │   │  0  │  1  │  2  │ 3 │ + │  ,  │
 //    └─────┴─────┼─────┼─────┼─────┴─────────┘   └─────┴─────┼─────┼───┼───┴─────┘
 //                │     │     │                               │  0  │ . │
 //                └─────┼─────┼─────┬─────────┐   ┌─────┬─────┼─────┼───┘
@@ -70,7 +71,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_NUM] = LAYOUT(
   KC_TRNS , KC_F12 , KC_F7   , KC_F8   , KC_F9   , EE_CLR  ,     KC_TRNS , KC_7    , KC_8    , KC_9   , KC_ASTR , KC_SLASH,
   KC_TRNS , KC_F11 , KC_F4   , KC_F5   , KC_F6   , KC_TRNS ,     KC_TRNS , KC_4    , KC_5    , KC_6   , KC_MINS , KC_TRNS ,
-  KC_TRNS , KC_F10 , KC_F1   , KC_F2   , KC_F3   , QK_BOOT ,     KC_0    , KC_1    , KC_2    , KC_3   , KC_PLUS , KC_TRNS ,
+  KC_TRNS , KC_F10 , KC_F1   , KC_F2   , KC_F3   , QK_BOOT ,     KC_0    , KC_1    , KC_2    , KC_3   , KC_PLUS , KC_COMMA,
                      KC_TRNS , KC_TRNS ,                                             KC_0    , KC_DOT                     ,
                                KC_TRNS , KC_TRNS , KC_TRNS ,     KC_TRNS , KC_TRNS , KC_TRNS
 ),
